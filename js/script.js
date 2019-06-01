2c627fa4d0428eaa72c705ec40945a9b5fda93ea
brian <brayomumo>
2019-06-01 16:35:06 +0300

    Create variabless to fetch user data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function tracker(){
-    
+    var sideA =parseInt(document.getElementById("sideA").value);
+    var sideB = parseInt(document.getElementById("sideB").value);
+    var sideC =parseInt(document.getElementById("sideC").value);
+
+    var results=document.querySelector("#final");
 }
