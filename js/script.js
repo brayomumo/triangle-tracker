42daebf3f5ec74da5c3a8e0399e22d05069035fa
brian <brayomumo>
2019-06-01 16:39:10 +0300

    Add if-else functions

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ function tracker(){
     var sideC =parseInt(document.getElementById("sideC").value);
 
     var results=document.querySelector("#final");
+    if(sideA <=0 && sideB<=0 && sideC <=0){
+        results.innerHTML="values cannot be null";
+    }else if((sideA==sideB && sideB!=sideC ) || (sideA!=sideB && sideC==sideA) || (sideC==sideB && sideC!=sideA)){
+        results.innerHTML="isosceles";
+    }else if(sideA!=sideB && sideB!=sideC && sideC!=sideA){
+        results.innerHTML="scalene";
+    
+    }else if (sideA ==sideB && sideB==sideC) {
+
+        results.innerHTML="Equilateral";
+    }
 }
